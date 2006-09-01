7b3cb1895d9f21baef21d7bf94b9337cd21d93da
Christian Persch <chpe@src.gnome.org>
2006-09-01 12:59:10 +0000

    Use UTF-8 copyright symbol

--- src/Listener.h
+++ src/Listener.h
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2003, 2004, 2005 Christian Persch
+ *  Copyright © 2003, 2004, 2005 Christian Persch
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU Lesser General Public License as published by
